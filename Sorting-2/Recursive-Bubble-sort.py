f626ca071490052beb95fe3b917ec71996a29cbf
user.name "Shivam Gaikwad <user.email "gaikwadshivam572@gmail.com>
2026-07-02 21:56:43 +0530

    Recursive bubble sort updated

--- Sorting-2/Recursive-Bubble-sort.py
+++ Sorting-2/Recursive-Bubble-sort.py
@@ -3,17 +3,14 @@ def bubble_sort(arr, n):
     if n == 1:
         return
 
-    # Perform one pass to push the largest to the end
     for j in range(n - 1):
         if arr[j] > arr[j + 1]:
-            # Swap elements
             arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     # Recurse for the rest of the array
     bubble_sort(arr, n - 1)
 
 
-# Main driver
 arr = [13, 46, 24, 52, 20, 9]
 n = len(arr)
 
